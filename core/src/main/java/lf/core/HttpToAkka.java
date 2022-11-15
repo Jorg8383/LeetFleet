54fb934dbd921e2d49bd7ad2479c3c498b00466b
flintdk <92164637+flintdk@users.noreply.github.com>
2022-11-15 12:37:33 +0000

    22/11/15 TK; Minor tweaks for jedis config.

--- core/src/main/java/lf/core/HttpToAkka.java
+++ core/src/main/java/lf/core/HttpToAkka.java
@@ -24,7 +24,12 @@ import redis.clients.jedis.providers.PooledConnectionProvider;
 public class HttpToAkka extends AllDirectives {
   private static final Logger log = LogManager.getLogger(HttpToAkka.class);
 
+  private static String hostname = "localhost";  // Sensible defaults
+  private static int port = 8080;
+
   public static void main(String[] args) throws Exception {
+    configFromArgs(args);
+
     // boot up server using the route as defined below
     ActorSystem<Void> system = ActorSystem.create(Behaviors.empty(), "routes");
 
@@ -34,25 +39,30 @@ public class HttpToAkka extends AllDirectives {
     // define.
     HttpToAkka app = new HttpToAkka();
 
-    final CompletionStage<ServerBinding> binding = http.newServerAt("core", 8080)
+    final CompletionStage<ServerBinding> binding = http.newServerAt(hostname, port)
         .bind(app.createRoute());
 
-    System.out.println("Server online at http://core:8080/ on the Docker Network.\nPress RETURN to stop...");
+    System.out.println("Server online at http://" + hostname + ":" + port + "/ on the Docker Network.\nPress RETURN to stop...");
 
     // Testing jedis connection - to be moved to vehicle actor
     Vehicle truck = new Vehicle("1", "1");
 
     // JedisPooled jedis = new JedisPooled("host.docker.internal", 6379);
-    HostAndPort config = new HostAndPort("host.docker.internal", 6379);
+    HostAndPort config = new HostAndPort(hostname, 6379);
     PooledConnectionProvider provider = new PooledConnectionProvider(config);
     UnifiedJedis client = new UnifiedJedis(provider);
 
-    // client.jsonSet("vehicle:111", truck);
+    client.jsonSet("vehicle:111", truck);
+    log.info("client ->", client);
+    Object fred = client.jsonGet("vehicle:111");
+    log.info("fred ->", fred);
 
     // System.out.println(client.jsonGet("vehicle:111"));
 
     client.set("planets", "Venus");
     System.out.println(client.get("planets"));
+
+    client.close();
 
     // *** WE NEED TO FIGURE OUT HOW TO SHUT IT DOWN - EASIEST WAY (INSECURE!!) IS
     // WITH URL
@@ -69,4 +79,28 @@ public class HttpToAkka extends AllDirectives {
 
         get(() -> complete("<h1>Say hello to akka-http</h1>"))));
   }
+
+  /* Parse args - populate required configuration */
+  private static void configFromArgs(String[] args) {
+    // Check the command line args for manual host/port configuration
+    for (int i = 0; i < args.length; i++) {
+        switch (args[i]) {
+            case "-h":
+                hostname = args[++i];
+                break;
+            case "-p":
+                port = Integer.parseInt(args[++i]);
+                break;
+            default:
+                System.out.println("Unknown flag: " + args[i] + "\n");
+                System.out.println("Valid flags are:");
+                System.out.println("\t-b <Broker Hostname>\tSpecify the hostname of the broker");
+                System.exit(0);
+        }
+    }
+    log.info("hostname:" + hostname);
+    log.info("port:" + port);
+
+    return;
+}
 }

71c6d3813c0511acda3d12d28fbfe87bd8eb6426
DanielGresak <80643624+DanielGresak@users.noreply.github.com>
2022-11-15 11:55:02 +0000

    DG Jedis keyvalue working, json not working

--- core/src/main/java/lf/core/HttpToAkka.java
+++ core/src/main/java/lf/core/HttpToAkka.java
@@ -14,6 +14,12 @@ import akka.http.javadsl.server.AllDirectives;
 import java.util.concurrent.CompletionStage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import lf.core.Vehicle;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPooled;
+import redis.clients.jedis.UnifiedJedis;
+import redis.clients.jedis.providers.PooledConnectionProvider;
 
 public class HttpToAkka extends AllDirectives {
   private static final Logger log = LogManager.getLogger(HttpToAkka.class);
@@ -24,27 +30,43 @@ public class HttpToAkka extends AllDirectives {
 
     final Http http = Http.get(system);
 
-    //In order to access all directives we need an instance where the routes are define.
+    // In order to access all directives we need an instance where the routes are
+    // define.
     HttpToAkka app = new HttpToAkka();
 
-    final CompletionStage<ServerBinding> binding =
-      http.newServerAt("core", 8080)
-          .bind(app.createRoute());
+    final CompletionStage<ServerBinding> binding = http.newServerAt("core", 8080)
+        .bind(app.createRoute());
 
     System.out.println("Server online at http://core:8080/ on the Docker Network.\nPress RETURN to stop...");
 
-    // *** WE NEED TO FIGURE OUT HOW TO SHUT IT DOWN - EASIEST WAY (INSECURE!!) IS WITH URL
+    // Testing jedis connection - to be moved to vehicle actor
+    Vehicle truck = new Vehicle("1", "1");
+
+    // JedisPooled jedis = new JedisPooled("host.docker.internal", 6379);
+    HostAndPort config = new HostAndPort("host.docker.internal", 6379);
+    PooledConnectionProvider provider = new PooledConnectionProvider(config);
+    UnifiedJedis client = new UnifiedJedis(provider);
+
+    // client.jsonSet("vehicle:111", truck);
+
+    // System.out.println(client.jsonGet("vehicle:111"));
+
+    client.set("planets", "Venus");
+    System.out.println(client.get("planets"));
+
+    // *** WE NEED TO FIGURE OUT HOW TO SHUT IT DOWN - EASIEST WAY (INSECURE!!) IS
+    // WITH URL
     // System.in.read(); // let it run until user presses return
 
     // binding
-    //     .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
-    //     .thenAccept(unbound -> system.terminate()); // and shutdown when done
+    // .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
+    // .thenAccept(unbound -> system.terminate()); // and shutdown when done
   }
 
   private Route createRoute() {
     return concat(
         path("hello", () ->
-            get(() ->
-                complete("<h1>Say hello to akka-http</h1>"))));
+
+        get(() -> complete("<h1>Say hello to akka-http</h1>"))));
   }
 }
